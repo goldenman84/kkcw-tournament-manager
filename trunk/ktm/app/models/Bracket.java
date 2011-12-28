9326179ad0a5396241a33f8f9cff099909bf1f1b
Cahit Atilgan <cahit.atilgan82@gmail.com>
2011-12-28 19:07:09 +0100

    Added fight, fighter models and defined relations.

--- trunk/ktm/app/models/Bracket.java
+++ trunk/ktm/app/models/Bracket.java
@@ -10,12 +10,12 @@ import java.util.*;
 
 @Entity
 public class Bracket extends Model {
-
+	
 	public String name;
-
+	
 	@ManyToOne
 	public Round round;
-
+	
 	public Bracket(Round round, String name) {
 		this.round = round;
 		this.name = name;
--- trunk/ktm/test/BasicTest.java
+++ trunk/ktm/test/BasicTest.java
@@ -76,52 +76,113 @@ public class BasicTest extends UnitTest {
 				Category.EliminationMode.Double).save();
 		Category medium = new Category(tournament, "Medium",
 				Category.EliminationMode.Double).save();
-		
+
 		// create new rounds
 		new Round(piccolo).save();
 		new Round(medium).save();
 		new Round(medium).save();
-		
+
 		// retrieve rounds by category
 		List<Round> piccoloRounds = Round.find("byCategory", piccolo).fetch();
 		assertEquals(1, piccoloRounds.size());
-		
+
 		List<Round> mediumRounds = Round.find("byCategory", medium).fetch();
 		assertEquals(2, mediumRounds.size());
-		
+
 		Round piccoloRound = piccoloRounds.get(0);
 		assertEquals("Piccolo", piccoloRound.category.name);
-		
+
 		Round mediumRound = mediumRounds.get(0);
 		assertEquals("Medium", mediumRound.category.name);
 	}
-	
+
 	@Test
 	public void createBrackets() {
 		Tournament tournament = new Tournament("Winti Cup 2012", new Date())
-		.save();
+				.save();
 		Category piccolo = new Category(tournament, "Piccolo",
 				Category.EliminationMode.Double).save();
-		
+
 		Round round = new Round(piccolo).save();
-		
+
 		new Bracket(round, "Winner Bracket").save();
 		new Bracket(round, "Looser Bracket").save();
-		
+
 		// retrieve rounds by category
 		List<Round> piccoloRounds = Round.find("byCategory", piccolo).fetch();
 		assertEquals(1, piccoloRounds.size());
-		
+
 		Round piccoloRound = piccoloRounds.get(0);
 		assertEquals("Piccolo", piccoloRound.category.name);
-		
+
 		List<Bracket> brackets = Bracket.find("byRound", piccoloRound).fetch();
 		assertEquals(2, brackets.size());
-		
+
 		Bracket winnerBracket = brackets.get(0);
 		assertEquals("Winner Bracket", winnerBracket.name);
-		
+
 		Bracket looserBracket = brackets.get(1);
 		assertEquals("Looser Bracket", looserBracket.name);
+	}
+
+	@Test
+	public void createFights() {
+		Tournament tournament = new Tournament("Winti Cup 2012", new Date())
+				.save();
+		Category piccolo = new Category(tournament, "Piccolo",
+				Category.EliminationMode.Double).save();
+
+		Round round = new Round(piccolo).save();
+		Bracket bracket = new Bracket(round, "Winner Bracket").save();
+
+		new Fight(bracket).save();
+		List<Fight> fights = Fight.find("byBracket", bracket).fetch();
+		assertEquals(1, fights.size());
+
+		new Fight(bracket).save();
+		assertEquals(1, fights.size());
+	}
+	
+	@Test
+	public void createFighters() {
+		Tournament tournament = new Tournament("Winti Cup 2012", new Date())
+				.save();
+		Category piccolo = new Category(tournament, "Piccolo",
+				Category.EliminationMode.Double).save();
+
+		Round round = new Round(piccolo).save();
+		Bracket bracket = new Bracket(round, "Winner Bracket").save();
+
+		Fight fightOne = new Fight(bracket).save();
+		Fighter mike = new Fighter("Mike", "Tyson", 45, 190).save();
+		Fighter evander = new Fighter("Evander", "Holyfield", 38, 188).save();
+		
+		List<Fighter> allFighters = Fighter.findAll();
+		assertEquals(2, allFighters.size());
+		
+		fightOne.addFighter(mike);
+		fightOne.addFighter(evander);
+		
+		assertEquals(2, fightOne.fighters.size());
+		
+		Fight fightTwo = new Fight(bracket).save();
+		
+		Fighter vladimir = new Fighter("Vladimir", "Klitchko", 38, 195).save();
+		fightTwo.addFighter(mike);
+		fightTwo.addFighter(vladimir);
+		
+		assertEquals(2, fightTwo.fighters.size());
+		
+		List<Fight> fights = Fight.findAll();
+		assertEquals(2, fights.size());
+		
+		Fight firstFight = fights.get(0);
+		assertEquals(2, firstFight.fighters.size());
+		
+		Fighter firstFighter = firstFight.fighters.get(0);
+		assertEquals("Mike", firstFighter.firstname);
+		assertEquals("Tyson", firstFighter.lastname);
+		assertEquals(45, firstFighter.age);
+		assertEquals(190, firstFighter.size);
 	}
 }
