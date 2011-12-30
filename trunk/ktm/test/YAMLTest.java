e3447e68640a7744f83379d88ee89506bc3b643f
felix <felix@muggi-VMLM>
2011-12-30 15:53:30 +0100

    changed relation fighter - fights, ManyToMany in Fight.java

--- trunk/ktm/test/YAMLTest.java
+++ trunk/ktm/test/YAMLTest.java
@@ -16,6 +16,8 @@ public class YAMLTest extends UnitTest {
 	
 	@Test
 	public void loadAndVerifyYAMLData() {
+		
+		// Load tournament state data from YML
 		Fixtures.loadModels("data.yml");
 		
 		// Count things
@@ -25,6 +27,13 @@ public class YAMLTest extends UnitTest {
 		assertEquals(3,Round.count());
 		assertEquals(2,Category.count());
 		assertEquals(1,Tournament.count());
+		
+		// Assert first fight / fighters
+		Fighter bobFighter = Fighter.all().first();
+		Fight bobFight = Fight.all().first();
+		assertEquals("bob",bobFighter.firstname);
+		assertEquals(2,bobFight.fighters.size());
+		assertEquals("bob",bobFight.fighters.get(0).firstname);
 		
 		// Get all rounds of the Piccolo category
 		List<Round> piccoloRounds = Round.find("category.name", "Piccolo").fetch();		
@@ -47,9 +56,9 @@ public class YAMLTest extends UnitTest {
 		assertEquals(2,firstFighters.size());
 		assertEquals(2,lastFighters.size());
 		assertEquals("bob",firstFighters.get(0).firstname);
-		assertEquals("han",firstFighters.get(0).firstname);
+		assertEquals("han",firstFighters.get(1).firstname);
 		assertEquals("bill",lastFighters.get(0).firstname);
-		assertEquals("alice",lastFighters.get(0).firstname);		
+		assertEquals("alice",lastFighters.get(1).firstname);		
 	}
 	
 }
