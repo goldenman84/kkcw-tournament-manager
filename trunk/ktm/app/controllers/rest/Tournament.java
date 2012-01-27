19dee642d7a973e5236656648b4f7447ad6677e1
Cahit Atilgan <cahit.atilgan82@gmail.com>
2012-01-27 22:28:13 +0100

    Added a deserializer method into controllers.rest.Tournament class.

--- trunk/ktm/app/controllers/rest/Tournament.java
+++ trunk/ktm/app/controllers/rest/Tournament.java
@@ -5,6 +5,7 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 
+import models.*;
 import controllers.rest.factories.DateFactory;
 import flexjson.JSONDeserializer;
 
@@ -14,7 +15,7 @@ public class Tournament extends REST {
 		List<models.Tournament> tournaments = models.Tournament.findAll();
 		REST.renderJSON(tournaments, REST.getDefaultSerializer());
 	}
-	
+
 	public static void create(models.Tournament newTournament) {
 		newTournament.save();
 		REST.renderJSON(newTournament, REST.getDefaultSerializer());
@@ -22,27 +23,23 @@ public class Tournament extends REST {
 
 	public static void show(Long id) {
 		models.Tournament tournament = models.Tournament.findById(id);
-		notFoundIfNull(tournament, "Couldn't find tournament (id: "+ id +") in database");
+		notFoundIfNull(tournament, "Couldn't find tournament (id: " + id + ") in database");
 		REST.renderJSON(tournament, REST.getDefaultSerializer());
 	}
 
 	public static void categories(Long id) {
 		models.Tournament tournament = models.Tournament.findById(id);
-		notFoundIfNull(tournament, "Couldn't find tournament (id: "+ id +") in database");
-		
+		notFoundIfNull(tournament, "Couldn't find tournament (id: " + id + ") in database");
+
 		List<models.Category> categories = models.Category.find("tournament", tournament).fetch();
 		REST.renderJSON(categories, REST.getDefaultSerializer());
 	}
-	
-	public static models.Tournament deserialize(String content) {
-		ArrayList<HashMap<String, Object>> tournaments = (ArrayList<HashMap<String, Object>>) new JSONDeserializer()
-				.use(Date.class, new DateFactory()).deserialize(content);
 
-		HashMap<String, Object> tournament = tournaments.get(0);
-		
-		Long id =  Long.valueOf((Integer) tournament.get("id"));
-		String name = (String) tournament.get("name");
-		Date date =  new Date((Long) tournament.get("date"));
-		return new models.Tournament(id, name, date);
+	public static models.Tournament deserialize(String content) {
+		ArrayList<models.Tournament> tournaments = (ArrayList<models.Tournament>) new JSONDeserializer()
+				.use(Date.class, new DateFactory()).deserialize(content);
+		models.Tournament tournament = tournaments.get(0);
+
+		return tournament;
 	}
 }
