176099afedf9e2b6cf20257254ab2c8b302edf0e
Cahit Atilgan <cahit.atilgan82@gmail.com>
2012-02-12 14:22:47 +0100

    Added REST handler for POST requests for models.Result instances.

--- trunk/ktm/app/controllers/rest/Result.java
+++ trunk/ktm/app/controllers/rest/Result.java
@@ -1,5 +1,6 @@
 package controllers.rest;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import play.mvc.*;
@@ -9,6 +10,20 @@ public class Result extends REST {
 	public static void index() {
 		List<models.Result> results = models.Result.findAll();
 		REST.renderJSON(results, REST.getDefaultSerializer());
+	}
+	
+	public static void create() {
+		String body = params.all().get("body")[0];
+		validation.required(body);
+		if (validation.hasErrors()) {;
+			response.status = 400;
+			renderJSON(validation.errors().get(0).message("body content"));
+		}
+		
+		ArrayList<models.Result> results = REST.deserialize(body);
+		models.Result result = results.get(0);
+		result.save();
+		REST.renderJSON(result, REST.getDefaultSerializer());
 	}
 	
 	public static void show(Long id) {
