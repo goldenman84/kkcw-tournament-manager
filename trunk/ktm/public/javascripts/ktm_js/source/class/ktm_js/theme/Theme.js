bc7b9d6a2a58ac69ebe07f8fc4f522f366c96220
Cahit Atilgan <catilgan@futurelab.ch>
2012-02-28 08:27:05 +0100

    Added the initial JavaScript frontend application using the qooxdoo framework.

---
+++ trunk/ktm/public/javascripts/ktm_js/source/class/ktm_js/theme/Theme.js
@@ -0,0 +1,21 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("ktm_js.theme.Theme",
+{
+  meta :
+  {
+    color : ktm_js.theme.Color,
+    decoration : ktm_js.theme.Decoration,
+    font : ktm_js.theme.Font,
+    icon : qx.theme.icon.Tango,
+    appearance : ktm_js.theme.Appearance
+  }
+});
