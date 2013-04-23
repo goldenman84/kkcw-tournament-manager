00849c873554bbc01f2ad81e5e2eb42e6aebdcd4
Cahit Atilgan <cahit.atilgan82@gmail.com>
2013-04-23 22:19:47 +0200

    Fixed the main application test.

--- ktm/test/ApplicationTest.java
+++ ktm/test/ApplicationTest.java
@@ -1,17 +1,23 @@
-import org.junit.*;
-import play.test.*;
-import play.mvc.*;
-import play.mvc.Http.*;
-import models.*;
+import static org.fest.assertions.Assertions.assertThat;
+import static play.mvc.Http.Status.OK;
+import static play.test.Helpers.charset;
+import static play.test.Helpers.contentAsString;
+import static play.test.Helpers.contentType;
+import static play.test.Helpers.status;
 
-public class ApplicationTest extends FunctionalTest {
+import org.junit.Test;
 
-    @Test
-    public void testThatIndexPageWorks() {
-        Response response = GET("/");
-        assertIsOk(response);
-        assertContentType("text/html", response);
-        assertCharset(play.Play.defaultWebEncoding, response);
-    }
-    
+import play.mvc.Result;
+
+public class ApplicationTest {
+
+	@Test
+	public void testThatIndexPageWorks() {
+		Result result = controllers.Application.index();
+
+		assertThat(status(result)).isEqualTo(OK);
+		assertThat(contentType(result)).isEqualTo("text/html");
+		assertThat(charset(result)).isEqualTo("utf-8");
+		assertThat(contentAsString(result)).contains("Welcome to the Karate Tournament Manager");
+	}
 }
