63c5dfdcd5ea837c0e83fa01a2431cccc700389b
Cahit Atilgan <cahit.atilgan82@gmail.com>
2013-04-23 23:18:40 +0200

    Sending objects directly without encapsulating to array in REST responses

--- ktm/app/controllers/rest/REST.java
+++ ktm/app/controllers/rest/REST.java
@@ -4,8 +4,7 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-import play.mvc.*;
-import play.mvc.Result;
+import play.mvc.Controller;
 import controllers.rest.factories.DateFactory;
 import flexjson.JSONDeserializer;
 import flexjson.JSONSerializer;
@@ -22,10 +21,9 @@ public class REST extends Controller {
 		if (model == null) {
 			model = new Object();
 		}
-		ArrayList<Object> list = new ArrayList<Object>();
-		list.add(model);
-		String json = REST.toJsonString(list);
-		return ok(json);
+
+		String json = REST.toJsonString(model);
+		return ok(json, "utf-8").as("application/json");
 	}
 
 	protected static play.mvc.Result renderJSON(List<?> models, JSONSerializer defaultSerializer) {
@@ -39,22 +37,23 @@ public class REST extends Controller {
 	}
 
 	public static JSONSerializer getDefaultSerializer() {
-		return new JSONSerializer().exclude("*.entityId",
-				"*.persistent");
+		return new JSONSerializer().exclude("*.entityId", "*.persistent");
 	}
 
 	public static <T> ArrayList<T> deserialize(String content) {
-		ArrayList<T> list = (ArrayList<T>) new JSONDeserializer()
-				.use(Date.class, new DateFactory()).deserialize(content);
+		ArrayList<T> list = (ArrayList<T>) new JSONDeserializer().use(Date.class, new DateFactory())
+				.deserialize(content);
 		return list;
 	}
 
 	/**
-	 * Parses the JSON string HTML response body and converts this JSON data to valid
-	 * play.db.jpa.Model instances.
-	 * @param {play.mvc.Scope.Params} params The response parameters received by the controller
-	 *                                       instance.
-	 * @return {ArrayList} An ArrayList with model instances, converted from JSON data.
+	 * Parses the JSON string HTML response body and converts this JSON data to
+	 * valid play.db.jpa.Model instances.
+	 * 
+	 * @param {play.mvc.Scope.Params} params The response parameters received by
+	 *        the controller instance.
+	 * @return {ArrayList} An ArrayList with model instances, converted from JSON
+	 *         data.
 	 */
 	public static <T> ArrayList<T> parseBodyJson() {
 		String body = request().body().asText();
